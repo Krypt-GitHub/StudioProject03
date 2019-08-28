f0562cfca2c712c57a665b74527e7d7cf150c17f
Krypt <irwenyap2001@gmail.com>
2019-08-29 04:26:56 +0800

    Remove gun texture

--- Base/StudioProject/SceneBase.cpp
+++ Base/StudioProject/SceneBase.cpp
@@ -189,7 +189,7 @@ void SceneBase::Init()
 	meshList[GEO_FLOOR] = MeshBuilder::GenerateCube("cube", Color(1, 1, 1), 1.f);
 	meshList[GEO_WALL] = MeshBuilder::GenerateCube("cube", Color(0.45, 0.45, 0.45), 1.f);
 	meshList[GEO_BULLET] = MeshBuilder::GenerateOBJ("bullet", "OBJ//bullet.obj");
-	meshList[GEO_BULLET]->textureArray[0] = LoadTGA("Image//black.tga");
+	meshList[GEO_BULLET]->textureArray[0] = LoadTGA("Image//gun.tga");
 	meshList[GEO_TRACER] = MeshBuilder::GenerateCylinder("tracer", Color(1, 0, 0), 10, 10, 1, 1);
 	meshList[GEO_PA_GUNSHATTER] = MeshBuilder::GenerateQuad("gunshatter", Color(), 1.f);
 	meshList[GEO_PA_GUNSHATTER]->textureArray[0] = LoadTGA("Image//PistolTri.tga");
